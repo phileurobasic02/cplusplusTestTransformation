8ae8544726dbd2379b26db32d8b4f08ac1b344c4
phileurobasic02 <138648330+phileurobasic02@users.noreply.github.com>
2023-07-06 08:14:33 +0200

    Create transformations.h

---
+++ transformations.h
@@ -0,0 +1,12 @@
+#ifndef TRANSFORMATIONS_H
+#define TRANSFORMATIONS_H
+
+#include <string>
+
+class Transformations {
+public:
+    static std::string capitalize(const std::string& text);
+    static std::string reverse(const std::string& text);
+};
+
+#endif
